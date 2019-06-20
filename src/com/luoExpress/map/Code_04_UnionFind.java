46f7623799b6455f6c3e48909fed4a28059e4b0e
DavidLuo06 <luoyu0811@gmail.com>
2019-06-20 15:27:09 -0400

    added comment

--- src/com/luoExpress/map/Code_04_UnionFind.java
+++ src/com/luoExpress/map/Code_04_UnionFind.java
@@ -2,6 +2,7 @@ package com.luoExpress.map;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Stack;
 
 public class Code_04_UnionFind {
 
@@ -10,30 +11,47 @@ public class Code_04_UnionFind {
 	}
 
 	public static class UnionFindSet {
+		//参数中，第一个node指当前的node，第二个指其父节点是谁 key：child，value：father
 		public HashMap<Node, Node> fatherMap;
+		//某一个节点，它所在的集合一共有多少个节点
 		public HashMap<Node, Integer> sizeMap;
 
-		public UnionFindSet() {
-			fatherMap = new HashMap<Node, Node>();
-			sizeMap = new HashMap<Node, Integer>();
+		public UnionFindSet(List<Node> nodes) {
+			makeSets(nodes);
 		}
 
 		public void makeSets(List<Node> nodes) {
-			fatherMap.clear();
-			sizeMap.clear();
+			fatherMap = new HashMap<Node, Node>();
+			sizeMap = new HashMap<Node, Integer>();
 			for (Node node : nodes) {
-				fatherMap.put(node, node);
+				fatherMap.put(node, node);//每个node自己形成一个集合 代表节点
 				sizeMap.put(node, 1);
 			}
 		}
 
 		private Node findHead(Node node) {
+			Stack<Node> stack = new Stack<>();
+			Node cur = node;
+			Node parent = fatherMap.get(cur);
+			while (cur!=parent){
+				stack.push(cur);
+				cur = parent;
+				parent = fatherMap.get(cur);
+			}
+			while (!stack.isEmpty()){
+				fatherMap.put(stack.pop(),parent);
+			}
+
+			return parent;
+			/*
 			Node father = fatherMap.get(node);
 			if (father != node) {
 				father = findHead(father);
 			}
 			fatherMap.put(node, father);
 			return father;
+		*/
+
 		}
 		
 		public boolean isSameSet(Node a, Node b) {
