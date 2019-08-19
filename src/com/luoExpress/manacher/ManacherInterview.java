4d155380fa9c74164e297fe89dc611d01a49fe35
isisisisisitch <dallucus@gmail.com>
2019-08-19 11:30:38 -0400

    added manacher interview

--- src/com/luoExpress/manacher/ManacherInterview.java
+++ src/com/luoExpress/manacher/ManacherInterview.java
@@ -1,8 +1,8 @@
 package com.luoExpress.manacher;
 
-public class Manacher {
-
+public class ManacherInterview {
     public static char[] manacherString(String str){
+
         char[] charArr = str.toCharArray();
         char[] res = new char[str.length()*2+1];
         int index = 0;
@@ -13,20 +13,20 @@ public class Manacher {
         return res;
     }
 
-    public static int maxLcpsLength(String str){
+    public static String shortestAdd(String str){
         if (str == null || str.length()==0) {
-            return 0;
+            return null;
         }
         char[] charArr = manacherString(str);
         int[] pArr = new int[charArr.length];//回文半径数组
-        int C = -1;//对称中心
-        int R = -1;//右边界
-        int max = Integer.MIN_VALUE;
+        int index = -1;//对称中心
+        int pR = -1;//右边界
+        int maxContainsEnd = -1;
         for (int i = 0; i < charArr.length; i++) {
 
             //R>i 区分最大的2种情况，i在R的内部或外部
             //2*C - i 就是对称点i'的位置，pArr[2*C - i]表示i'点的回文区域  R-i表示i到右边界R的距离
-            pArr[i] = R>i?Math.min(pArr[2*C - i],R-i):1;
+            pArr[i] = pR>i?Math.min(pArr[2*index - i],pR-i):1;
             //i+pArr[i]<charArr.length 要验证的区域没有越界， i-pArr[i]>-1 左边区域也没越界，情况2，3 我也让它向外扩充
             while (i+pArr[i]<charArr.length && i-pArr[i]>-1){
                 if (charArr[i+pArr[i]]==charArr[i-pArr[i]]){////情况1，4，如果扩的结果相同，回文半径++
@@ -36,19 +36,32 @@ public class Manacher {
                 }
             }
 
-            if (i+pArr[i]>R){//如果扩的边界超过R
-                R=i+pArr[i];//就有了新的回文右边界了
-                C = i;//新的回文中心
+            if (i+pArr[i]>pR){//如果扩的边界超过R
+                pR=i+pArr[i];//就有了新的回文右边界了
+                index = i;//新的回文中心
             }
 
-            max = Math.max(max,pArr[i]);//记录最大的回文长度
+            if (pR == charArr.length) {//当整体的回文来到最右边界的时候
+                maxContainsEnd = pArr[i];
+                break;
+            }
+
         }
 
-        return max-1;
+        char[] res = new char[str.length()-maxContainsEnd+1];
+        for (int i = 0; i < res.length; i++) {
+            res[res.length-1-i] = charArr[i*2+1];
+        }
+
+        return String.valueOf(res);
+
+
     }
 
     public static void main(String[] args) {
-        String str1 = "abc1234321ab";
-        System.out.println(maxLcpsLength(str1));
+        String s = "abcd12321";
+        System.out.println(shortestAdd(s));
     }
+
+
 }
